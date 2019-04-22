f5b122fe6dd11c1c6b1037c8a73f97a637cf2459
Shivam1097 <shivam.dev1097@gmail.com>
2019-04-22 22:52:45 +0530

    Initial Commit

---
+++ JS Stalker Exercise/script.js
@@ -0,0 +1,27 @@
+
+
+// Just to load the html before the pop-ups , we put our code inside
+// window.setTimeout(function() {
+//     // put all of your JS code here
+// }, 500);
+// as follows ->
+
+// window.setTimeout(function() {
+//     var fname=prompt("Enter first name: ");
+//     var lname=prompt("Enter last name: ");
+//     var age=prompt("Enter age : ");
+//     alert("Welcome "+fname+" "+lname);
+//     alert("Your age is : "+age);
+//     console.log("Welcome "+fname+" "+lname);
+//     console.log("Your age is : "+age);
+// }, 500);
+
+
+
+var fname=prompt("Enter first name: ");
+var lname=prompt("Enter last name: ");
+var age=prompt("Enter age : ");
+alert("Welcome "+fname+" "+lname);
+alert("Your age is : "+age);
+console.log("Welcome "+fname+" "+lname);
+console.log("Your age is : "+age);
